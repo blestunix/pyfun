ba72d3c949d8338769f7f69e3fc47c72169c7c9a
blestunix <95473302+blestunix@users.noreply.github.com>
2022-01-20 22:44:36 +0530

    Guess the number
    A special date with a special program

---
+++ number_guess.py
@@ -0,0 +1,31 @@
+# Written by Saud Kadiri on 20th January 💛
+
+import random
+
+def main():
+    lo = int(input("Enter the lower bound: "))
+    hi = int(input("Enter the upper bound: "))
+
+    try:
+        num = random.randint(lo, hi)
+    except ValueError:
+        print("Invalid range")
+        exit(1)
+
+    attempts = 1
+    while True:
+        guess = int(input("Enter a number of your choice: "))
+        if guess == num:
+            print("You guessed it right!")
+            break
+        elif guess < num:
+            print("Low")
+        else:
+            print("High")
+        attempts += 1
+
+    print(f"You guessed the correct number in {attempts}.")
+
+
+if __name__ == '__main__':
+    main()
