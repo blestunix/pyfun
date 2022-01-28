c546cd25d232d02b8ae9a86d919b5d457c687236
blestunix <95473302+blestunix@users.noreply.github.com>
2022-01-28 21:12:17 +0530

    Insertion operations

--- DS/linkedList.py
+++ DS/linkedList.py
@@ -2,3 +2,19 @@ class Node:
   def __init__(self, val, nxt=None):
     self.val = val
     self.nxt = nxt
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+  
+  def insertHead(data):
+    self.head = Node(data, self.head)
+    
+  def insertTail(data):
+    if self.head is None:
+      return self.inserHead(data)
+    trav = self.head
+    while trav.next:
+      trav = trav.nxt
+    trav.next = Node(data)
+    
