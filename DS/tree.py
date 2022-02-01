57c815268607176ca8795f90e300ff697dd06c34
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-01 15:14:42 +0530

    Updated

--- DS/tree.py
+++ DS/tree.py
@@ -1,5 +1,6 @@
 class Tree:
-    def __init__(self):
-        self.left = None
-        self.right = None
+    def __init__(self, data, left=None, right=None):
         self.data = None
+        self.left = left
+        self.right = right
+        
