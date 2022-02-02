7688652efb83ce4ebee83912084f9f68a0a0cda8
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-02 15:36:34 +0530

    Functionality to have non-repeated child

--- DS/tree.py
+++ DS/tree.py
@@ -4,7 +4,10 @@ class Tree:
         self.children = []
         self.parent = None
 
-    def add_child(self, child):
+    def add_child(self, child, duplicates_allowed=True):
+        if not duplicates_allowed:
+            if child in self.children:
+                return
         child.parent = self
         self.children.append(child)
-   
+
