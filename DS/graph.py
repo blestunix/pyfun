3809eab92f23226d8d4b856002cb93fc28356224
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-04 21:03:19 +0530

    Find path between two vertices

--- DS/graph.py
+++ DS/graph.py
@@ -1,3 +1,15 @@
 class Graph:
   def __init__(self, graph={}):
     self.graph = graph
+
+  def find_path(self, start, end):
+    q = [start]
+    path = []
+    while q:
+      curr = q.pop(0)
+      if curr == end or end in self.graph[curr]:
+        return path
+      path.append(curr)
+      for connected in self.graph[curr]:
+        if connected not in q and connected not in path:
+          q.append(connected)
