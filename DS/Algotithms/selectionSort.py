0ad4cad5efb84ebaee72f4b3b6407193257c1f2b
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-05 19:27:44 +0530

    Complete selection sort program

--- DS/Algotithms/selectionSort.py
+++ DS/Algotithms/selectionSort.py
@@ -1,3 +1,7 @@
+# Name: It is so called because it selects the smallest/largest element in the list and swaps it with the element at the first position, 
+# then selects the next smallest/largest element in the list and swaps it with the element at the second position and so on until it is 
+# done with the number at last position in the list.
+
 def find_next_min(lst, i):
     mini = min(lst[i:])
     return lst.index(mini)
@@ -6,3 +10,8 @@ def selection_sort(lst):
     for i in range(len(lst)):
         j = find_next_min(lst, i)
         lst[i], lst[j] = lst[j], lst[i]   # swap        
+
+        
+lst = [45, 20, 34, 21, 81, 42]
+selection_sort(lst)
+print(lst)
